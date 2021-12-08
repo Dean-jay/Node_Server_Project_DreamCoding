93ff3e77d2882b63d4f5867819978b3c4e449351
Dean-jay <80184397+Dean-jay@users.noreply.github.com>
2021-12-08 17:00:09 +1100

    checkout merge conflict

--- router/tweets.js
+++ router/tweets.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import 'express-async-errors';
-import getController from '../controller/get.js';
-import getIdController from '../controller/getId.js';
-import postController from '../controller/post.js';
-import putController from '../controller/put.js';
-import deleteController from '../controller/delete.js';
+// import getController from '../controller/get.js';
+// import getIdController from '../controller/getId.js';
+// import postController from '../controller/post.js';
+// import putController from '../controller/put.js';
+// import deleteController from '../controller/delete.js';
 
 const router = express.Router();
 
-router.get('/', getController);
-router.get('/:id', getIdController);
-router.post('/', postController);
-router.put('/:id', putController);
-router.delete('/:id', deleteController);
+// router.get('/', getController);
+// router.get('/:id', getIdController);
+// router.post('/', postController);
+// router.put('/:id', putController);
+// router.delete('/:id', deleteController);
 
 export default router;
